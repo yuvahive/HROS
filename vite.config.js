da61a274f2020cd24555c85d0bcd8dfb1526dd6f
himanshu140505 <himanshuyadav4596@gmail.com>
2026-05-24 06:06:28 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/HROS/',
+  build: {
+    target: 'esnext',
+  },
   server: {
     port: 3000,
     host: true,
